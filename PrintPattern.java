24ae8a2ece02e2698a2a56e6755b07ffd9195e6f
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-07-11 09:36:26 +0530

    03 challenge

---
+++ PrintPattern.java
@@ -0,0 +1,42 @@
+/**
+ * 03_Print_Pattern
+ */
+
+public class PrintPattern {
+    public static void main(String[] args) {
+
+// 01 pattern        
+        System.out.println(" * ");
+        System.out.println(" * * ");
+        System.out.println(" * * * ");
+        System.out.println(" * * * *");
+        System.out.println(" * * * * *");
+// Or
+        System.out.println("\n * \n * * \n * * * \n * * * * \n * * * * *");
+
+// 02 pattern   
+
+       System.out.println(" * * * * *");
+       System.out.println(" * * * *");
+        System.out.println(" * * * ");
+        System.out.println(" * * ");
+        System.out.println(" * ");
+// Or
+
+      System.out.println("\n * * * * * \n * * * * \n * * * \n * * \n * ");
+   
+// 02 pattern      
+       
+      System.out.println("         * ");
+      System.out.println("       * * ");
+      System.out.println("     * * * ");
+      System.out.println("   * * * * ");
+      System.out.println(" * * * * * ");
+
+// or 
+
+     System.out.println("\n         * \n       * * \n     * * * \n   * * * * \n * * * * *");
+
+    }
+    
+}
