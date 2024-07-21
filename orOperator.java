68a451ce04a9a496d54aeeffe75835b688aa281d
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-07-21 23:18:14 +0530

    22 Chall.......

---
+++ orOperator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class orOperator {
+public static void main(String[] args) {
+   Scanner input = new Scanner(System.in);
+   System.out.println("********Showcasing bitwise nd Operator**********");  
+   System.out.println("Enter your first choice number: ");
+   int first = input.nextInt();
+   System.out.println("Enter your second choice number: ");  
+   int secont = input.nextInt();
+
+   int result = first | secont; 
+   System.out.println("your Result= " + result);
+}    
+    
+}
